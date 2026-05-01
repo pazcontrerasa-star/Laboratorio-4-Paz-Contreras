95014b047200c1d847ca3bf30fa881198a640639
Your Name <you@example.com>
2026-05-01 17:24:08 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -189,7 +189,7 @@ Pair * firstTreeMap(TreeMap * tree) {
 
 Pair * nextTreeMap(TreeMap * tree) {
     TreeNode * temp = tree->current;
-    TreeNode * sig = temp->curent->right;
+    TreeNode * sig = temp->current->right;
     if(sig == NULL)return NULL;
     tree->current = sig;
     return tree->current->pair;
